c40675d90033f8ec7b81cdd55b356fc445db764d
amillert <devmood.business@gmail.com>
2022-01-28 19:03:32 +0100

    add retry extension syntax

--- src/main/scala/pl/amillert/retry/policy/utils/syntax.scala
+++ src/main/scala/pl/amillert/retry/policy/utils/syntax.scala
@@ -1,7 +1,10 @@
 package pl.amillert.retry.policy.utils.syntax
 
+import cats.effect.IO
 import cats.FlatMap
 import cats.syntax.functor._
+
+import scala.concurrent.duration.FiniteDuration
 
 extension [F[_]: FlatMap, A](fa: F[A])
 
@@ -10,3 +13,21 @@ extension [F[_]: FlatMap, A](fa: F[A])
     t = Thread.currentThread().getName()
     _ = println(s"[$t] $a")
   } yield a
+
+object RetrySyntaxOps:
+  import pl.amillert.retry.policy.domain.ExponentialBackoffRetry
+
+  extension [A](io: IO[A])
+
+    def retry(maxRetry: Int, giveUpAfter: FiniteDuration, onFail: A => IO[A]): IO[A] =
+      new ExponentialBackoffRetry().retry(maxRetry, giveUpAfter, onFail)(io)
+
+  extension [A](io: IO[A])
+
+    def retry(maxRetry: Int, giveUpAfter: FiniteDuration): IO[A] =
+      new ExponentialBackoffRetry().retry(maxRetry, giveUpAfter)(io)
+
+  extension [A](io: IO[A])
+    def retry(maxRetry: Int): IO[A] = new ExponentialBackoffRetry().retry(maxRetry)(io)
+
+  extension [A](io: IO[A]) def retry: IO[A] = new ExponentialBackoffRetry().retry()(io)
