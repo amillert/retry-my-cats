27054e5f92e8f102452f87de8706390247eadd13
amillert <devmood.business@gmail.com>
2022-02-22 00:05:22 +0100

    move all extension methods under single  keyword

--- src/main/scala/pl/amillert/retry/policy/utils/syntax.scala
+++ src/main/scala/pl/amillert/retry/policy/utils/syntax.scala
@@ -1,8 +1,9 @@
 package pl.amillert.retry.policy.utils.syntax
 
-import cats.effect.IO
 import cats.FlatMap
 import cats.syntax.functor._
+
+import cats.effect.IO
 
 import scala.concurrent.duration.FiniteDuration
 
@@ -22,12 +23,9 @@ object RetrySyntaxOps:
     def retry(maxRetry: Int, giveUpAfter: FiniteDuration, onFail: A => IO[A]): IO[A] =
       new ExponentialBackoffRetry().retry(maxRetry, giveUpAfter, onFail)(io)
 
-  extension [A](io: IO[A])
-
     def retry(maxRetry: Int, giveUpAfter: FiniteDuration): IO[A] =
       new ExponentialBackoffRetry().retry(maxRetry, giveUpAfter)(io)
 
-  extension [A](io: IO[A])
     def retry(maxRetry: Int): IO[A] = new ExponentialBackoffRetry().retry(maxRetry)(io)
 
-  extension [A](io: IO[A]) def retry: IO[A] = new ExponentialBackoffRetry().retry()(io)
+    def retry: IO[A] = new ExponentialBackoffRetry().retry()(io)
